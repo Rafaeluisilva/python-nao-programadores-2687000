d8c1b57219af4ff961bc959866ffbc5633bb7a69
Rafaeluisilva <rafael_luis@outlook.com>
2026-01-22 14:01:14 +0000

    Resolução variaveis

--- exercicios/1-primeiros-passos/1-variaveis.py
+++ exercicios/1-primeiros-passos/1-variaveis.py
@@ -4,8 +4,21 @@
 # - E não podemos usar palavras reservadas do Python, como if, for, from, etc. 
 
 # Declare variáveis do tipo string
+nome = 'Rafael_Luis_Silva'
 
+profissao = "Contador"
+
+objetivo = '''
+Aprender
+Python para
+Evoluir
+'''
 # Declare variáveis de cada tipo numérico
+numero_inteiro = 100
 
+numero_decimal = 150.25
 
 # Declare uma variável do tipo booleano
+verdadeiro = True
+
+falso = False
